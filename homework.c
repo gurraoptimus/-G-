cc756c82e957e08ae1e0f02e071d99e37eb326f7
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2022-06-10 12:57:30 +0200

    update

--- homework.c
+++ homework.c
@@ -1,1 +1,11 @@
-{G}
+/*
+Assignment:
+
+	Using floating point numbers, prove that:
+
+	0.1 + 0.2 = 0.3
+
+	BONUS POINTS if you use 17 digits of accuracy
+
+	AFTER 3 FAILED ATTEMPTS WE WILL POWER DOWN YOUR COMPUTER
+*/
